faa4a98bb14f748a084dbbded0aad0f746236456
slamach <drslampro@gmail.com>
2020-04-09 00:47:49 +0700

    Logger added

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -1,8 +1,12 @@
 package server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
     public static final int PORT = 1821;
     public static final int CONNECTION_TIMEOUT = 60*1000;
+    public static Logger logger = LogManager.getLogger("ServerLogger");
 
     public static void main(String[] args) {
         Server server = new Server(PORT, CONNECTION_TIMEOUT);
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -32,15 +32,18 @@ public class Server {
                     processClientRequest(clientSocket);
                 } catch (ConnectionErrorException exception) {
                     Outputer.println("Повторное соединение с клиентом...");
+                    App.logger.info("Повторное соединение с клиентом...");
                 } catch (SocketTimeoutException exception) {
                     break;
                 } catch (IOException exception) {
                     Outputer.printerror("Произошла ошибка при попытке завершить соединение с клиентом!");
+                    App.logger.error("Произошла ошибка при попытке завершить соединение с клиентом!");
                 }
             }
             stop();
         } catch (OpeningServerSocketException exception) {
             Outputer.printerror("Сервер не может быть запущен!");
+            App.logger.fatal("Сервер не может быть запущен!");
         }
     }
 
@@ -49,24 +52,31 @@ public class Server {
             if (serverSocket == null) throw new ClosingSocketException();
             serverSocket.close();
             Outputer.println("Работа сервера успешно завершена.");
+            App.logger.info("Работа сервера успешно завершена.");
         } catch (ClosingSocketException exception) {
             Outputer.printerror("Невозможно завершить работу еще не запущенного сервера!");
+            App.logger.error("Невозможно завершить работу еще не запущенного сервера!");
         } catch (IOException exception) {
             Outputer.printerror("Произошла ошибка при завершении работы сервера!");
+            App.logger.error("Произошла ошибка при завершении работы сервера!");
         }
     }
 
     private void openServerSocket() throws OpeningServerSocketException {
         try {
             Outputer.println("Запуск сервера...");
+            App.logger.info("Запуск сервера...");
             serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(soTimeout);
             Outputer.println("Сервер успешно запущен.");
+            App.logger.info("Сервер успешно запущен.");
         } catch (IllegalArgumentException exception) {
             Outputer.printerror("Порт " + port + " находится за пределами возможных значений!");
+            App.logger.fatal("Порт " + port + " находится за пределами возможных значений!");
             throw new OpeningServerSocketException();
         } catch (IOException exception) {
             Outputer.printerror("Произошла ошибка при попытке использовать порт " + port + "!");
+            App.logger.fatal("Произошла ошибка при попытке использовать порт " + port + "!");
             throw new OpeningServerSocketException();
         }
     }
@@ -74,14 +84,18 @@ public class Server {
     private Socket connectToClient() throws ConnectionErrorException, SocketTimeoutException {
         try {
             Outputer.println("Прослушивание порта " + port + "...");
+            App.logger.info("Прослушивание порта " + port + "...");
             Socket clientSocket = serverSocket.accept();
             Outputer.println("Соединение с клиентом успешно установлено.");
+            App.logger.info("Соединение с клиентом успешно установлено.");
             return clientSocket;
         } catch (SocketTimeoutException exception) {
             Outputer.printerror("Превышено время ожидания подключения!");
+            App.logger.warn("Превышено время ожидания подключения!");
             throw new SocketTimeoutException();
         } catch (IOException exception) {
             Outputer.printerror("Произошла ошибка при соединении с клиентом!");
+            App.logger.error("Произошла ошибка при соединении с клиентом!");
             throw new ConnectionErrorException();
         }
     }
@@ -99,13 +113,17 @@ public class Server {
                 clientWriter.writeObject(responseToUser);
                 clientWriter.flush();
                 Outputer.println("Запрос успешно обработан.");
+                App.logger.info("Запрос успешно обработан.");
             }
         } catch (ClassNotFoundException exception) {
             Outputer.printerror("Произошла ошибка при чтении полученных данных!");
+            App.logger.error("Произошла ошибка при чтении полученных данных!");
         } catch (InvalidClassException | NotSerializableException exception) {
             Outputer.printerror("Произошла ошибка при отправке данных на клиент!");
+            App.logger.error("Произошла ошибка при отправке данных на клиент!");
         } catch (IOException exception) {
             Outputer.printerror("Непредвиденный разрыв соединения с клиентом!");
+            App.logger.warn("Непредвиденный разрыв соединения с клиентом!");
         }
     }
 }
